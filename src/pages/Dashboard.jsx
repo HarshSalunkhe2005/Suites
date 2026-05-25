fe79c0c45a731aa56fe8e7133169d1738b7ae1a8
HarshSalunkhe2005 <nightfuryharsh@gmail.com>
2026-05-25 11:34:27 +0530

    update

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -15,7 +15,7 @@ export default function Dashboard() {
              <svg xmlns="http://www.w3.org/2000/svg" width="48" height="48" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><path d="M4 14.899A7 7 0 1 1 15.71 8h1.79a4.5 4.5 0 0 1 2.5 8.242"></path><path d="M12 12v9"></path><path d="m8 17 4 4 4-4"></path></svg>
           </div>
           <h3>Compress Image</h3>
-          <p>Compress JPG, PNG, and WebP instantly using exact target sizes.</p>
+          <p>Compress JPG, PNG, and JPEG instantly using exact target sizes.</p>
         </Link>
         
         <Link to="/crop" className="tool-card">
